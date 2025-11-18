b194be0a501b170f0c0a94e018a79d6c1efd1612
maybenotpanda <maybenotpanda@gmail.com>
2025-11-19 05:27:05 +0700

    [create-table]: add new dir

--- app/database/migrations/create_log.py
+++ app/database/migrations/create_log.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+sys.path.append(ROOT_DIR)
+
 from app.config.database import engine
 
 import app.models.logs as logs
